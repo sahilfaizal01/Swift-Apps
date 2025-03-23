736c14540707151851bf9449fa449a31c2db49f3
sahilfaizal01 <sahilfaizal0704@gmail.com>
2025-03-23 13:07:26 -0700

    Initial Commit

---
+++ Resume App/Resume_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resume_AppApp.swift
+//  Resume App
+//
+//  Created by Sahil Faizal on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Resume_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Resume AppTests/Resume_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Resume_AppTests.swift
+//  Resume AppTests
+//
+//  Created by Sahil Faizal on 23/03/25.
+//
+
+import Testing
+@testable import Resume_App
+
+struct Resume_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
